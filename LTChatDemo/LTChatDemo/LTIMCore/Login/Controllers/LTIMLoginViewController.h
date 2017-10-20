b9069c942bb0cd4af28aee52566f4caaf755cae0
liangtong <l900416@163.com>
2017-10-20 15:58:18 +0800

    Add Demo

---
+++ LTChatDemo/LTChatDemo/LTIMCore/Login/Controllers/LTIMLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTIMLoginViewController.h
+//  LTIM
+//
+//  Created by 梁通 on 2017/9/22.
+//  Copyright © 2017年 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTIMLoginViewController : UIViewController
+
+@end
---
+++ LTChatDemo/LTChatDemo/LTIMCore/Roster/Controllers/LTIMRosterTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  LTIMRosterTableViewController.h
+//  LTIM
+//
+//  Created by 梁通 on 2017/9/21.
+//  Copyright © 2017年 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/*花名册*/
+@interface LTIMRosterTableViewController : UITableViewController
+
+@end
